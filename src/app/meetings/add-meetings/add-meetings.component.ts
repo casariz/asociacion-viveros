5bd708ddfc467f156d215a5b9351a31af0830d93
Daniel Casallas <danielfelipedfco@gmail.com>
2024-06-17 17:26:30 -0500

    Agregar formato al formulario para añadir reunión

--- src/app/meetings/add-meetings/add-meetings.component.ts
+++ src/app/meetings/add-meetings/add-meetings.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-add-meetings',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-meetings.component.html',
   styleUrl: './add-meetings.component.css'
 })
 export class AddMeetingsComponent {
+  meetingForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.meetingForm = this.fb.group({
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      topic: ['', Validators.required],
+      location: ['', Validators.required],
+      room: ['', Validators.required],
+      organizer: ['', Validators.required],
+      participants: ['', Validators.required],
+      agenda: ['', Validators.required],
+      notes: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.meetingForm.valid) {
+      // Lógica para manejar la sumisión del formulario
+    }
+  }
 
 }
