18a0068e882465267ab1c58b8bf1b787c89e6d44
Daniel Casallas <danielfelipedfco@gmail.com>
2024-06-27 11:39:44 -0500

    Agregar servicio para topics

---
+++ src/app/services/topics.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TopicsService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) { }
+
+  getTopics(): Observable<any>{
+    return this.http.get(`${this.apiUrl}/topics`);
+  }
+
+  createTopic(topic: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/topics`, topic);
+  } 
+
+  deleteTopic(id:number, topic: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/topics/${id}/delete`, topic);
+  } 
+}
